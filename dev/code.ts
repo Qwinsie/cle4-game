fe21cfb4753ff4401f08cd360d1a5ced89206eb6
Qwinsie <theqpowerrr@gmail.com>
2020-05-25 14:52:14 +0200

    Added code.ts for creating codewolkjes. (Note: Not scaled yet)

---
+++ dev/code.ts
@@ -0,0 +1,22 @@
+class Code {
+
+    private code : HTMLElement
+    public x : number
+    public y : number
+
+    constructor() {
+        this.createCode()
+    }
+
+    createCode() {
+        this.code = document.createElement("code")
+
+        let game =document.getElementsByTagName("game")[0]
+        game.appendChild(this.code)
+
+        this.x = 0
+        this.y = 0
+
+        this.code.style.transform = `translate(${this.x}px, ${this.y}px)`
+    }
+}
