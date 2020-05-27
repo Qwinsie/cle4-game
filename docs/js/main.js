04d20566d7911c060e117907cd4894da3b163d7f
Dvbritsem <dvbritsem@gmail.com>
2020-05-27 11:31:21 +0200

    Editted main

--- docs/js/main.js
+++ docs/js/main.js
@@ -54,6 +54,70 @@ class Enemy1 {
         this.enemy1.style.transform = `translate(${this.x}px, ${this.y}px)`;
     }
 }
+class Enemy2 {
+    constructor() {
+        this.space = false;
+        this.leftspeed = 0;
+        this.rightspeed = 0;
+        this.y_velo = 0;
+        this.jumping = true;
+        this.enemy2 = document.createElement("enemy2");
+        let game = document.getElementsByTagName("game")[0];
+        game.appendChild(this.enemy2);
+        this.leftkey = 90;
+        this.rightkey = 67;
+        this.spaceKey = 88;
+        this.x = 1000;
+        this.y = 600;
+        window.addEventListener("keydown", (e) => this.onKeyDown(e));
+        window.addEventListener("keyup", (e) => this.onKeyUp(e));
+    }
+    onKeyDown(e) {
+        switch (e.keyCode) {
+            case this.leftkey:
+                this.leftspeed = 10;
+                break;
+            case this.rightkey:
+                this.rightspeed = 10;
+                break;
+            case this.spaceKey:
+                this.space = true;
+                break;
+        }
+    }
+    onKeyUp(e) {
+        switch (e.keyCode) {
+            case this.leftkey:
+                this.leftspeed = 0;
+                break;
+            case this.rightkey:
+                this.rightspeed = 0;
+                break;
+            case this.spaceKey:
+                this.space = false;
+                break;
+        }
+    }
+    update() {
+        if (this.space && this.jumping == false) {
+            this.y_velo -= 60;
+            this.jumping = true;
+        }
+        this.y_velo += 1.2;
+        this.y += this.y_velo;
+        this.y_velo *= 0.9;
+        if (this.y > 600) {
+            this.jumping = false;
+            this.y = 600;
+            this.y_velo = 0;
+        }
+        let newX = this.x - this.leftspeed + this.rightspeed;
+        if (newX > 0 && newX + 100 < (1440 - this.enemy2.clientWidth)) {
+            this.x = newX;
+        }
+        this.enemy2.style.transform = `translate(${this.x}px, ${this.y}px)`;
+    }
+}
 class Game {
     constructor() {
         this.canvas = document.createElement("canvas");
@@ -62,11 +126,13 @@ class Game {
         this.robot = new Robot;
         this.tree = new Tree;
         this.enemy1 = new Enemy1;
+        this.enemy2 = new Enemy2;
         this.code = new Code;
         this.gameLoop();
     }
     gameLoop() {
         this.enemy1.update();
+        this.enemy2.update();
         this.robot.update();
         requestAnimationFrame(() => this.gameLoop());
     }
@@ -87,7 +153,7 @@ class Robot {
         this.rightKey = 39;
         this.spaceKey = 32;
         this.x = 200;
-        this.y = 630;
+        this.y = 600;
         window.addEventListener("keydown", (e) => this.onKeyDown(e));
         window.addEventListener("keyup", (e) => this.onKeyUp(e));
     }
@@ -133,9 +199,9 @@ class Robot {
         this.y += this.y_velo;
         this.x_velo *= 0.9;
         this.y_velo *= 0.9;
-        if (this.y > 550 - 16 - 32) {
+        if (this.y > 600 - 16 - 32) {
             this.jumping = false;
-            this.y = 550 - 16 - 32;
+            this.y = 600 - 16 - 32;
             this.y_velo = 0;
         }
         if (this.x < -200) {
