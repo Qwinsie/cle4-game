ab304aba48f9f22aa67a2904d3a9a537e5eca4fd
Qwinsie <theqpowerrr@gmail.com>
2020-05-27 12:59:44 +0200

    Edited code.ts for collision.

--- dev/code.ts
+++ dev/code.ts
@@ -1,8 +1,11 @@
 class Code {
 
     private code : HTMLElement
+
     public x : number
     public y : number
+
+    public collected : boolean = false
 
     constructor() {
         this.createCode()
@@ -14,9 +17,27 @@ class Code {
         let game =document.getElementsByTagName("game")[0]
         game.appendChild(this.code)
 
-        this.x = 0
-        this.y = 0
+        this.x = 500
+        this.y = 200
 
-        this.code.style.transform = `translate(${this.x}px, ${this.y}px) scale(0.1)`
+        this.code.style.transform = `translate(${this.x}px, ${this.y}px) scale(0.2)`
+    }
+
+    public getRectangle() {
+        return this.code.getBoundingClientRect()
+    }
+
+    public getFutureRectangle(){
+        let rect = this.code.getBoundingClientRect()
+        return rect
+    }
+
+    public update() : void {
+
+        if(this.collected) {
+            console.log("collected")
+            this.code.remove()
+            this.collected = false
+        }
     }
 }
