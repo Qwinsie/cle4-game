9b2b5dbdbe91ac9c7678f27fc0acac4d0b4ff5dd
Qwinsie <theqpowerrr@gmail.com>
2020-05-29 19:14:51 +0200

    removed movement functions + added automatic movement to left + cleaned code

--- dev/enemy1.ts
+++ dev/enemy1.ts
@@ -1,67 +1,52 @@
 class Enemy1 {
 
     private enemy1 : HTMLElement
-    public x : number
-    public y : number
+    private _x: number = 0
+    private _y: number = 0
 
-    private leftkey: number
-    private rightkey: number
+    public get x(): number {
+        return this._x
+    }
+
+    public get y(): number {
+        return this._y
+    }
 
     private leftspeed : number = 0
     private rightspeed : number = 0
 
     public canvas : HTMLElement
 
-    constructor() {
-        this.enemy1 = document.createElement("enemy1")
+    constructor(x:number,y:number) {
+        this.createEnemy1(x,y)
+    }
 
+    private createEnemy1(x:number,y:number) {
+        this.enemy1 = document.createElement("enemy1")
         let game =document.getElementsByTagName("game")[0]
         game.appendChild(this.enemy1)
-        // 65 = A, 68 = D
-        this.leftkey = 65
-        this.rightkey = 68
 
-        this.x = 1200
-        this.y = 630
+        this._x = x
+        this._y = y
 
-        window.addEventListener("keydown", (e: KeyboardEvent) => this.onKeyDown(e))
-        window.addEventListener("keyup", (e: KeyboardEvent) => this.onKeyUp(e))
-
-        this.enemy1.style.transform = `translate(${this.x}px, ${this.y}px)`
+        this.enemy1.style.transform = `translate(${this._x}px, ${this._y}px)`
     }
 
     public getRectangle() {
         return this.enemy1.getBoundingClientRect()
     }
 
-    private onKeyDown(e: KeyboardEvent): void {
-        switch (e.keyCode) {
-            case this.leftkey:
-                this.leftspeed = 10
-                break
-            case this.rightkey:
-                this.rightspeed = 10
-                break
-        }
-    }
-
-    private onKeyUp(e: KeyboardEvent): void {
-        switch (e.keyCode) {
-            case this.leftkey:
-                this.leftspeed = 0
-                break
-            case this.rightkey:
-                this.rightspeed = 0
-                break
-        }
-    }
-
     public update() {
-        let newX = this.x - this.leftspeed + this.rightspeed
+    this.leftspeed =+ 1
+
+    let newX = this._x - this.leftspeed + this.rightspeed
         // check of het monster binnen het beeld blijft
-        if (newX > 0 && newX < (1440 - this.enemy1.clientWidth)){
-         this.x = newX
+        if (newX < (1440 - this.enemy1.clientWidth)){
+         this._x = newX
         }
-        this.enemy1.style.transform = `translate(${this.x}px, ${this.y}px)`
+        if (newX < 0 - this.enemy1.clientWidth) {
+            this.enemy1.remove()
+        }
+        this.enemy1.style.transform = `translate(${this._x}px, ${this._y}px)`
     }
 }
