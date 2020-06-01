bd65d700275540f34411afb001e09e3b499af55d
Dvbritsem <dvbritsem@gmail.com>
2020-06-01 10:29:41 +0200

    Added autojump to enemy2

--- docs/js/main.js
+++ docs/js/main.js
@@ -113,14 +113,13 @@ class Enemy2 {
         }
     }
     update() {
-        if (this.space && this.jumping == false) {
-            this.yVelo -= 50;
+        if (this.jumping == false) {
+            this.yVelo -= 40;
             this.jumping = true;
         }
-        this.yVelo += 1.2;
+        this.yVelo += 1.0;
         this._y += this.yVelo;
-        this.yVelo *= 0.95;
-
+        this.yVelo *= 0.98;
         if (this._y > 600) {
             this.jumping = false;
             this._y = 600;
