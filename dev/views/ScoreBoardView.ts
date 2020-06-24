3b753751cc0ab2a93e4dd6218b2f4c7069390820
Qwinsie <theqpowerrr@gmail.com>
2020-06-24 13:55:26 +0200

    Scoreboardview change (Not in use)

--- dev/views/ScoreBoardView.ts
+++ dev/views/ScoreBoardView.ts
@@ -1,50 +1,28 @@
 class ScoreBoardView {
 
-    private createform : HTMLElement
-
-    private $form = null;
-    private  $nameField = null;
-    private  $scoreField = null;
-    private currentScore = null;
-
-    constructor(score:number) {
-        console.log("ScoreBoardView created");
-        
-        // this.currentScore = null;
-        // // getAllScores()
+    private createform
+    private form = document.querySelector('form');
+    private input = document.getElementById('item');
+    private item = localStorage.getItem('name');
+    private currentscore : number = 0
     
-        // // Check if localStorage werkt
-        if (typeof window.localStorage === "undefined") {
-            console.error('Local storage is not available in your browser');
-            return;
-        }
+    constructor(score:number) {
+        this.currentscore = score
 
         this.createForm(score)
-    
-        // // Get Name + Score from LocalStorage
-        // let currentScore = this.getScore()
-        // let name = localStorage.getItem('name')
-        // console.log(currentScore);
-    
-        // // Add Name + Score in Leaderboard
-        // this.addScore(name, currentScore)
-    
-        // // Get HTML Tags from Form
-        // this.$form = document.getElementById('form');
-        // this.$nameField = document.getElementById('name');
-        // this.$scoreField = document.getElementById('score');
-    
-        // this.fillFieldsFromLocalStorage();
-    
-        // // Send Data to LocalStorage
-        this.createform.addEventListener('submit', this.submitHandler);
+        
+        this.form.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        localStorage.setItem('name', this.input.value);
+        this.input.value = "";
+        });
+        
     }
 
     public createForm(score: number) {
-        this.createform = document.createElement("form")
 
-        this.createform.setAttribute("action", ""); // Setting Action Attribute on Form
-        this.createform.setAttribute("method", "post"); // Setting Method Attribute on Form
+        this.createform = document.createElement("form")
         
         let game = document.getElementsByTagName("game")[0]
         game.appendChild(this.createform)
@@ -64,8 +42,8 @@ class ScoreBoardView {
         this.createform.appendChild(namelabel);
 
         var inputelement = document.createElement('input'); // Create Input Field for Name
+        inputelement.setAttribute("id", "item");
         inputelement.setAttribute("type", "text");
-        inputelement.setAttribute("name", "dname");
         this.createform.appendChild(inputelement);
 
         var linebreak = document.createElement('br');
@@ -78,97 +56,167 @@ class ScoreBoardView {
         var heading2 = document.createElement('h2'); // Create Input Field for Name
         heading2.innerHTML = `${score}`
         this.createform.appendChild(heading2);
-
-        var linebreak = document.createElement('br');
-        this.createform.appendChild(linebreak);
-
-        var submitelement = document.createElement('input'); // Append Submit Button
-        submitelement.setAttribute("type", "submit");
-        submitelement.setAttribute("name", "dsubmit");
-        submitelement.setAttribute("value", "Submit");
-        this.createform.appendChild(submitelement);
-
-        // this.$form = document.getElementById('form');
         
-    }
-
-
-    private addScore(name, score) {
-        console.log(name + " " + score);
-
-            let list = document.querySelector("#scores")
-            
-            const newli = document.createElement("li")
-            newli.innerHTML = `${name} ${score}`
-    }
-
-    private getScore() {
-        let score = localStorage.getItem('score')
-        if(score) {
-            return JSON.parse(score)
-        } else {
-            return []
-        }
-    }
-
-    private fillFieldsFromLocalStorage() {
-        if (localStorage.getItem('name') !== null) {
-            this.$nameField.value = localStorage.getItem('name');
-            this.$scoreField.value = localStorage.getItem('score');
-        }
-    }
-
-    private  submitHandler(e) {
-        e.preventDefault();
-        localStorage.setItem('name', this.$nameField.value);
-        // localStorage.setItem('score', $scoreField.value);
-
-        this.currentScore.push(this.$scoreField.value)
-        localStorage.setItem('score', JSON.stringify(this.currentScore))
     }
 }
-// function getAllScoresErrorHandler(data) {
-//     console.log(data)
-// }
 
-// function getAllScores() {
-//     fetch('../json/leaderboard.json')
-//     .then(function(response){
-//         if(response.status == 200 && response.ok){
-//             console.log("alles in orde!")
-//             return response.json() // JSON.parse
-//         }
-//         throw new Error(response.statusText)
-//         //OF
-//         console.log("Er gaat iets fout")
-//         // console.log(response.status)
-//         // console.log(response.ok)
-//     })
-//     .then(getAllScoresSuccesHandler)
-//     .catch(getAllScoresErrorHandler)
-// }
+//     private createform : HTMLElement
 
-// function getAllScoresSuccesHandler(data, currentScore) {
-//     console.log(currentScore);
-    
-//     results = data.results
-//     console.log(data.results);
-    
-//     for (let i = 0; i < results.length;i++){
-
-//         let h = document.querySelector("#title")
-//         h.innerHTML = "Leaderboard"
-
-//         let list = document.querySelector("#scores")
-        
-//         const newli = document.createElement("li")
-//         newli.innerHTML = `${results[i].name} ${results[i].score} ${currentScores}`
-//         list.appendChild(newli)
-
-//         window.addEventListener("click", doSomething)
-
-//         function doSomething(event) {
-//             console.log("You clicked somewhere")
-//         }
+//     private form = null;
+//     private  $nameField = null;
+//     private  $scoreField = null;
+//     private currentScore = null;
 //     }
 // }
+
+    /// OLD
+
+//     constructor(score:number) {
+//         console.log("ScoreBoardView created");
+        
+//         // this.currentScore = null;
+//         // // getAllScores()
+    
+//         // // Check if localStorage werkt
+//         if (typeof window.localStorage === "undefined") {
+//             console.error('Local storage is not available in your browser');
+//             return;
+//         }
+
+//         this.createForm(score)
+    
+//         // // Get Name + Score from LocalStorage
+    
+//         // // Add Name + Score in Leaderboard
+//         this.addScore(this.$nameField.value, score)
+    
+//         // // Get HTML Tags from Form
+//         this.form = document.getElementById('form');
+//         this.$nameField = document.getElementById('name');
+//         this.$scoreField = document.getElementById('score');
+    
+//         // this.fillFieldsFromLocalStorage();
+    
+//         // // Send Data to LocalStorage
+//         this.createform.addEventListener('submit', this.submitHandler(e) => {
+
+//         });
+//     }
+
+//     public createForm(score: number) {
+//         this.createform = document.createElement("form")
+
+//         this.createform.setAttribute("action", "../leaderboard.html"); // Setting Action Attribute on Form
+//         this.createform.setAttribute("method", "post"); // Setting Method Attribute on Form
+        
+//         let game = document.getElementsByTagName("game")[0]
+//         game.appendChild(this.createform)
+
+//         let heading = document.createElement('h2'); // Heading of Form
+//         heading.innerHTML = "You did it!";
+//         this.createform.appendChild(heading);
+
+//         var line = document.createElement('hr'); // Giving Horizontal Row After Heading
+//         this.createform.appendChild(line);
+
+//         var linebreak = document.createElement('br');
+//         this.createform.appendChild(linebreak);
+
+//         var namelabel = document.createElement('label'); // Create Label for Name Field
+//         namelabel.innerHTML = "Your Name : "; // Set Field Labels
+//         this.createform.appendChild(namelabel);
+
+//         var inputelement = document.createElement('input'); // Create Input Field for Name
+//         inputelement.setAttribute("type", "text");
+//         inputelement.setAttribute("name", "dname");
+//         this.createform.appendChild(inputelement);
+
+//         var linebreak = document.createElement('br');
+//         this.createform.appendChild(linebreak);
+
+//         var usernamelabel = document.createElement('label'); // Create Label for Address Field
+//         usernamelabel.innerHTML = "Score : "; // Set Field Labels
+//         this.createform.appendChild(usernamelabel);
+
+//         var heading2 = document.createElement('h2'); // Create Input Field for Name
+//         heading2.innerHTML = `${score}`
+//         this.createform.appendChild(heading2);
+
+//         var linebreak = document.createElement('br');
+//         this.createform.appendChild(linebreak);
+
+//         var submitelement = document.createElement('button'); // Append Submit Button
+//         submitelement.setAttribute("type", "submit");
+//         submitelement.setAttribute("name", "dsubmit");
+//         submitelement.setAttribute("value", "Submit");
+//         this.createform.appendChild(submitelement);
+
+//         this.form = document.getElementById('form');
+        
+//     }
+
+
+//     private addScore(name, score) {
+//         console.log(name + " " + score);
+
+//             let list = document.querySelector("#scores")
+            
+//             const newli = document.createElement("li")
+//             newli.innerHTML = `${name} ${score}`
+//     }
+
+//     private  submitHandler(e) {
+//         e.preventDefault();
+//         localStorage.setItem('name', this.$nameField.value);
+//         // localStorage.setItem('score', $scoreField.value);
+//         console.log("Submitted");
+
+//         this.currentScore.push(this.$scoreField.value)
+//         localStorage.setItem('score', JSON.stringify(this.currentScore))
+//     }
+// }
+// // function getAllScoresErrorHandler(data) {
+// //     console.log(data)
+// // }
+
+// // function getAllScores() {
+// //     fetch('../json/leaderboard.json')
+// //     .then(function(response){
+// //         if(response.status == 200 && response.ok){
+// //             console.log("alles in orde!")
+// //             return response.json() // JSON.parse
+// //         }
+// //         throw new Error(response.statusText)
+// //         //OF
+// //         console.log("Er gaat iets fout")
+// //         // console.log(response.status)
+// //         // console.log(response.ok)
+// //     })
+// //     .then(getAllScoresSuccesHandler)
+// //     .catch(getAllScoresErrorHandler)
+// // }
+
+// // function getAllScoresSuccesHandler(data, currentScore) {
+// //     console.log(currentScore);
+    
+// //     results = data.results
+// //     console.log(data.results);
+    
+// //     for (let i = 0; i < results.length;i++){
+
+// //         let h = document.querySelector("#title")
+// //         h.innerHTML = "Leaderboard"
+
+// //         let list = document.querySelector("#scores")
+        
+// //         const newli = document.createElement("li")
+// //         newli.innerHTML = `${results[i].name} ${results[i].score} ${currentScores}`
+// //         list.appendChild(newli)
+
+// //         window.addEventListener("click", doSomething)
+
+// //         function doSomething(event) {
+// //             console.log("You clicked somewhere")
+// //         }
+// //     }
+// // }
